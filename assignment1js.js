31e0954a16884a0391239c33cd016d2bfece9930
jhongover9000 <jhh508@nyu.edu>
2021-02-02 19:50:18 +0400

    initial commit
    just putting somehalf-made stuff in the website to see if it works

---
+++ assignment1js.js
@@ -0,0 +1,11 @@
+let root = assignment1css.css;
+
+//changing color of text as user progresses through website
+root.addEventListener("")
+
+//using location to update navigation
+
+//jquery for jumping to next element
+
+
+
